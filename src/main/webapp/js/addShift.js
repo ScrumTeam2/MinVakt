7b25c7e848910078d7ccc7ecb6f24154b801d103
olekau <olekau@student.hist.no>
2017-01-16 14:29:28 +0100

    Fixed some more shit

--- src/main/webapp/js/addShift.js
+++ src/main/webapp/js/addShift.js
@@ -3,48 +3,50 @@
  */
 
 
-function submitForm(form) {
-    var data = $(form).serializeArray()
-        .reduce(function(a, x) { a[x.name] = x.value; return a; }, {});
+$(document).ready(function() {
+    $('#createShiftBtn').on("click", function(e) {
+        e.preventDefault();
 
-    var id = -1;
-    var staffDay = parseInt(data.day);
-    var staffEvening = parseInt(data.evening);
-    var staffNight = parseInt(data.night);
-    var date = data.date;
-    var shiftUsers = null;
-    var deptId = parseInt(data.department);
+        var data = $('#shiftForm').serializeArray()
+            .reduce(function(a, x) { a[x.name] = x.value; return a; }, {});
 
-    var day = {
-        id: id,
-        staffNumb: staffDay,
-        date: date,
-        type: 1,
-        deptId: deptId,
-        shiftUsers: shiftUsers,
-    };
+        var id = -1;
+        var staffDay = parseInt(data.day);
+        var staffEvening = parseInt(data.evening);
+        var staffNight = parseInt(data.night);
+        var date = data.date;
+        var shiftUsers = null;
+        var deptId = parseInt(data.department);
 
-    console.log(JSON.stringify(day));
-    console.log(day);
+        var day = {
+            id: id,
+            staffNumb: staffDay,
+            date: date,
+            type: 1,
+            deptId: deptId,
+            shiftUsers: shiftUsers,
+        };
+
+        console.log(JSON.stringify(day));
+        console.log(day);
+
+        $.ajax({
+                url: "/rest/shift",
+                type: "POST",
+                dataType: "json",
+                contentType: "application/json",
+                data: JSON.stringify(day)
+            })
+            .done(function() {
+                console.log( "success" );
+            })
+            .fail(function(error) {
+                console.log( "error", error );
+            })
+            .always(function() {
+                console.log( "complete" );
+            });
 
 
-    $.ajax({
-        url: "/rest/shift",
-        type: "POST",
-        dataType: "json",
-        contentType: "application/json",
-        data: JSON.stringify(day)
-    })
-    .done(function() {
-        console.log( "success" );
-    })
-    .fail(function(error) {
-        console.log( "error", error );
-    })
-    .always(function() {
-        console.log( "complete" );
     });
-
-
-    return false;
-}
+});
