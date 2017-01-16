ac838918418b856bd83ff4b3028cb823263fdc63
olekau <olekau@student.hist.no>
2017-01-16 13:41:14 +0100

    Worked on ajax post method

--- src/main/webapp/js/addShift.js
+++ src/main/webapp/js/addShift.js
@@ -7,22 +7,44 @@ function submitForm(form) {
     var data = $(form).serializeArray()
         .reduce(function(a, x) { a[x.name] = x.value; return a; }, {});
 
-    console.log(data);
+    var id = -1;
+    var staffDay = parseInt(data.day);
+    var staffEvening = parseInt(data.evening);
+    var staffNight = parseInt(data.night);
+    var date = data.date;
+    var shiftUsers = null;
+    var deptId = parseInt(data.department);
+
+    var day = {
+        id: id,
+        staffNumb: staffDay,
+        date: date,
+        type: 1,
+        deptId: deptId,
+        shiftUsers: shiftUsers,
+    };
+
+    console.log(JSON.stringify(day));
+    console.log(day);
+
 
     $.ajax({
         url: "/rest/shift",
         type: "POST",
-        data: data
+        dataType: "json",
+        contentType: "application/json",
+        data: JSON.stringify(day)
     })
     .done(function() {
         console.log( "success" );
     })
-    .fail(function() {
-        console.log( "error" );
+    .fail(function(error) {
+        console.log( "error", error );
     })
     .always(function() {
         console.log( "complete" );
     });
+
 
     return false;
 }
