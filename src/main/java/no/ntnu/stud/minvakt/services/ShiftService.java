2e23fc86e6da23ad759891da205631217ec0a81f
Audun Arbo <audunwar@student.hist.no>
2017-01-23 10:11:30 +0100

    Fixed REST call

--- src/main/java/no/ntnu/stud/minvakt/services/ShiftService.java
+++ src/main/java/no/ntnu/stud/minvakt/services/ShiftService.java
@@ -127,7 +127,7 @@ public class ShiftService extends SecureService{
     @POST
     @Path("/{shiftId}/replaceuser")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response replaceEmployeeOnShift(@PathParam("shiftId") int shiftId, int oldUserId, int newUserId) {
+    public Response replaceEmployeeOnShift(@PathParam("shiftId") int shiftId, @FormParam("oldUserId") int oldUserId, @FormParam("newUserId") int newUserId) {
         if(getSession() == null || !getSession().isAdmin()) return null;
 
         boolean statusOk = shiftDB.replaceEmployeeOnShift(shiftId, oldUserId, newUserId);
