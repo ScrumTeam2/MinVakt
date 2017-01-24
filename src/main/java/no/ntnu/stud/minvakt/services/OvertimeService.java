826b60fb2dcb885030241d402a1e5faf40979667
--global <marit_holm1@hotmail.com>
2017-01-24 10:25:55 +0100

    OvertimeService/OvertimeServiceTest

--- src/main/java/no/ntnu/stud/minvakt/services/OvertimeService.java
+++ src/main/java/no/ntnu/stud/minvakt/services/OvertimeService.java
@@ -1,13 +1,17 @@
 package no.ntnu.stud.minvakt.services;
 
+import no.ntnu.stud.minvakt.data.NewsFeedItem;
 import no.ntnu.stud.minvakt.data.Overtime;
+import no.ntnu.stud.minvakt.database.NewsFeedDBManager;
 import no.ntnu.stud.minvakt.database.OvertimeDBManager;
+import no.ntnu.stud.minvakt.database.UserDBManager;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.sql.Timestamp;
 
 /**
  * Created by Marit on 20.01.2017.
@@ -15,45 +19,12 @@ import javax.ws.rs.core.Response;
 
 @Path("/overtime")
 public class OvertimeService extends SecureService{
-    OvertimeDBManager overtimeDBManager = new OvertimeDBManager();
+    OvertimeDBManager overtimeDBM = new OvertimeDBManager();
+    NewsFeedDBManager newsfeedDBM = new NewsFeedDBManager();
+    UserDBManager userDBM = new UserDBManager();
+
     public OvertimeService(@Context HttpServletRequest request) {
         super(request);
-    }
-
-
-    //GET fetch not approved overtime (used by Admin)
-    //getUnapporovedOvertime() return Overtime[]
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getUnapprovedOvertime(){
-        if(getSession().isAdmin()){
-            Overtime[] overtime = overtimeDBManager.getUnapprovedOvertime();
-            if(overtime==null){
-                return Response.status(400).entity("Unable to find unapproved overtime.").build();
-            }else{
-                return Response.status(200).entity(overtime).build();
-            }
-        }else {
-            return Response.status(Response.Status.UNAUTHORIZED).entity("User is not an admin").build();
-        }
-    }
-
-
-    //POST set approved overtime (used by Admin)
-    //approveOvertime(userId, shiftId) return true/false
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response setApprovedOvertime(@QueryParam("userId") int userId, @QueryParam("shiftId") int shiftId){
-        if(getSession().isAdmin()){
-            boolean isApproved = overtimeDBManager.approveOvertime(userId, shiftId);
-            if(!isApproved){
-                return Response.status(400).entity("Unable to find unapproved overtime.").build();
-            }else{
-                return Response.status(200).build();
-            }
-        }else {
-            return Response.status(Response.Status.UNAUTHORIZED).entity("User is not an admin").build();
-        }
     }
 
     //POST register overtime (used by employee-user)
@@ -62,8 +33,28 @@ public class OvertimeService extends SecureService{
     @Path("/{userId}")
     @Consumes(MediaType.APPLICATION_JSON)
     public Response setOvertime(Overtime overtime) {
+        boolean isRegistered = overtimeDBM.setOvertime(overtime.getUserId(), overtime.getShiftId(), overtime.getStartTime(), overtime.getMinutes());
 
-        return null;
+        if(isRegistered) {
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            String content = "Tekst";
+            int adminId = userDBM.getAdminId();
+
+            if(adminId==0){
+                return Response.status(400).entity("Overtime registered, but could not find admin user").build();
+            }else {
+                NewsFeedItem notification = new NewsFeedItem(-1, timestamp, content, adminId, overtime.getUserId(), overtime.getShiftId(), NewsFeedItem.NewsFeedCategory.TIMEBANK, overtime.getStartTime());
+                int newsfeedId = newsfeedDBM.createNotification(notification);
+
+                if (newsfeedId == 0){
+                    return Response.status(400).entity("Overtime registered, but sending notification to admin failed").build();
+                }else{
+                    return Response.status(200).build();
+                }
+            }
+        }else{
+            return Response.status(400).entity("Could not register overtime").build();
+        }
     }
 
     //GET fetch overtime for user (used by employee-user)
@@ -71,9 +62,36 @@ public class OvertimeService extends SecureService{
     @GET
     @Path("/{userId}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Overtime[] getOvertime(@PathParam("userId") int shiftId) {
+    public Overtime[] getOvertime(@PathParam("userId") int userId) {
+        Overtime[] overtime = overtimeDBM.getOvertimeByUserId(userId);
+        return overtime;
+    }
 
-        //if (getSession() == null) return null;
-        return null;
+    @DELETE
+    @Path("/{userId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteOvertime(@PathParam("userId") int userId, @QueryParam("shiftId") int shiftId, @QueryParam("startTime") int startTime) {
+
+        int feedId = newsfeedDBM.getNewsFeedIdThroughOvertime(userId, shiftId, startTime);
+        if(feedId<1){
+            return Response.status(400).entity("Could not find feedId").build();
+        }else{
+            boolean delNotification = newsfeedDBM.deleteNotification(feedId);
+
+            if (delNotification){
+                boolean isDeleted = overtimeDBM.deleteOvertime(userId, shiftId, startTime);
+
+                if(isDeleted){
+                    return Response.status(200).build();
+                }else{
+                    return Response.status(400).entity("Could not delete overtime").build();
+                }
+
+            }else{
+                return Response.status(400).entity("Could not delete overtime notification in newsfeed. Overtime ot deleted.").build();
+            }
+
+        }
+
     }
 }
--- src/test/java/no/ntnu/stud/minvakt/services/OvertimeServiceTest.java
+++ src/test/java/no/ntnu/stud/minvakt/services/OvertimeServiceTest.java
@@ -4,16 +4,16 @@ package no.ntnu.stud.minvakt.services;
  * Created by Marit on 23.01.2017.
  */
 
+import no.ntnu.stud.minvakt.data.Overtime;
 import org.json.JSONObject;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Ignore;
-import org.junit.Test;
+import org.junit.*;
 import org.springframework.mock.web.MockHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.core.Response;
 import java.sql.Date;
+
+import static org.junit.Assert.assertTrue;
 
 public class OvertimeServiceTest {
     private static OvertimeService overtimeService;
@@ -26,23 +26,45 @@ public class OvertimeServiceTest {
     }
 
     @Test
-    public void getUnapprovedOvertimeTest(){
+    public void setOvertimeTest() throws Exception {
+        int userId = 10;
+        int shiftId = 13;
+        int startTime = 900;
+        int minutes = 15;
+        Overtime overtime = new Overtime(userId, shiftId, startTime, minutes, false);
+        Response response = overtimeService.setOvertime(overtime);
+        Assert.assertEquals(200, response.getStatus());
+        overtimeService.deleteOvertime(userId, shiftId, startTime);
+    }
+
+    @Test
+    public void getOvertimeTest() throws Exception {
+        int userId = 4;
+        Overtime[] overtime = overtimeService.getOvertime(userId);
+        Overtime[] expRes = new Overtime[2];
+        expRes[0] = new Overtime(4,28,780,-120,false);
+        expRes[1] = new Overtime(4, 61, 840, -60, false);
+
+        Assert.assertEquals(expRes[0], overtime[0]);
+        Assert.assertEquals(expRes[1], overtime[1]);
+
+        /*
+         user_id, shift_id, start_time, minutes, approved
+	     	4 	    28  	    780 	-120 	    0
+	    	4 	    61 	        840 	 -60 	    0
+         */
 
     }
 
     @Test
-    public void setApprovedOvertime() throws Exception {
-
+    public void deleteOvertimeTest() throws Exception{
+        int userId = 10;
+        int shiftId = 13;
+        int startTime = 900;
+        int minutes = 15;
+        Overtime overtime = new Overtime(userId, shiftId, startTime, minutes, false);
+        overtimeService.setOvertime(overtime);
+        Response response = overtimeService.deleteOvertime(userId, shiftId, startTime);
+        Assert.assertEquals(200, response.getStatus());
     }
-
-    @Ignore
-    public void setOvertime() throws Exception {
-
-    }
-
-    @Ignore
-    public void getOvertime() throws Exception {
-
-    }
-
 }
