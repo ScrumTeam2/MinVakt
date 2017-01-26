78f34a39cd29618e1e3477f1582f39958ef15446
ingvildbro <ingvild.broen@gmail.com>
2017-01-26 13:31:49 +0100

    js and html for timebank form ready

--- src/main/webapp/js/overtimeScript.js
+++ src/main/webapp/js/overtimeScript.js
@@ -1,29 +1,140 @@
 /**
  * Created by AnitaKristineAune on 24.01.2017.
  */
-
 var shiftId = getUrlParameter("shiftId");
-var content = getUrlParameter("message");
+var date = getUrlParameter("date");
+var type = getUrlParameter("type");
 
 $(document).ready(function () {
     getShift();
+
+    var $hours = $('#hours');
+    var $minutes = $('#minutes');
+
+    $hours.removeClass('error').parent().attr('data-content', '');
+    $minutes.removeClass('error').parent().attr('data-content', '');
+
+    checkInput();
 });
 
 
-function sendTimebank(){
+function checkInput(){
+    $('#timebankBtn').click(function(e){
+        e.preventDefault();
 
+        console.log("klikket");
+
+        var formError = false;
+
+        var $hours = $('#hours');
+        var $minutes = $('#minutes');
+
+        $hours.removeClass('error').parent().attr('data-content', '');
+        $minutes.removeClass('error').parent().attr('data-content', '');
+
+        if(!$hours.val()){
+            $hours.addClass('error').parent().attr('data-content', 'Du må fylle inn timer.');
+            formError = true;
+        }
+        if(!$minutes.val()){
+            $minutes.addClass('error').parent().attr('data-content', 'Du må fylle inn minutter.');
+            formError = true;
+        }
+
+        var hoursVal = $hours.val();
+        var minutesVal = $minutes.val();
+
+        console.log(formError);
+
+        if(!formError){
+            calcMinutes(hoursVal, minutesVal);
+        }
+    });
+}
+
+function calcMinutes(hoursVal, minutesVal){
+    var minutes = hoursVal * 60 + minutesVal;
+    registerOvertime(minutes);
+}
+
+function sendTimebank(formData){
+    $.ajax({
+        url: "/rest/overtime",
+        type: 'POST',
+        dataType: 'json',
+        contentType: "application/json",
+        data: JSON.stringify(formData),
+        success: function(){
+            console.log("ok post");
+            popupContent();
+        },
+        error: function(){
+            console.log("ikke ok post");
+        }
+    });
 }
 
 function getShift(){
+    var $shiftInfo = $('.shift-info');
+    var message1 = "Mandag 19. januar 2017";
+    var message2 = "Dagvakt 07:00-15:00";
+    $shiftInfo.html(
+        `<p class="lead">${message1}</p>
+        <p class="sub">${message2}</p>`
+    );
 
 }
 
-function registerOvertime(){
-
+function registerOvertime(minutes){
+    var startTime;
+    /*switch (type){
+        case "DAY":
+            startTime = 900;
+            break;
+        case "EVENING":
+            startTime = 1380;
+            break;
+        case "NIGHT":
+            startTime = 420;
+            break;
+        default:
+            console.log("type not known", type);
+    }*/
+    var formData;
+    formData = {
+        "userId": -1,
+        "shiftId": 2,
+        "startTime": 900,
+        "minutes": minutes,
+        "approved": false,
+        "date": "2017-01-23",
+        "type": "DAY"
+    };
+    sendTimebank(formData);
 }
 
-function registerAbsence(){
+function popupContent(){
+    var $popup = $('.popup');
 
+    var message1 = "Mandag 19. januar 2017";
+    var message2 = "Dagvakt 07:00-15:00";
+    var hours = $('#hours').val();
+    var minutes = $('#minutes').val();
+
+    if(hours === 0){
+        $popup.html(
+            `<h3>Overtid sendt til godkjenning</h3>
+                <p>Vakt: ${message1} - ${message2}</p>
+                <p>Overtid: ${minutes} minutter</p>`
+        );
+    } else{
+        $popup.html(
+            `<h3>Overtid sendt til godkjenning</h3>
+                <p>Vakt: ${message1} - ${message2}</p>
+                <p>Overtid: ${hours} timer og ${minutes} minutter</p>`
+        );
+    }
+    showPopup();
 }
 
 function showPopup(){
@@ -35,5 +146,4 @@ function showPopup(){
 function hidePopup(){
     var $popup = $('.popup');
     $popup.hide();
-
 }
