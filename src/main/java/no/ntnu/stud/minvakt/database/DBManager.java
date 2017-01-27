d78714791f446694e000410d61a3c17358b06b59
Audun Arbo <audunwar@student.hist.no>
2017-01-27 23:58:21 +0100

    Added config file in WEB-INF/MinVakt.propterties. Added connection pool

--- src/main/java/no/ntnu/stud/minvakt/database/DBManager.java
+++ src/main/java/no/ntnu/stud/minvakt/database/DBManager.java
@@ -86,6 +86,15 @@ public abstract class DBManager{
         closeConnection();
     }
 
+    protected void finallyStatement(ResultSet res) {
+        try {
+            if (res != null) res.close();
+        } catch (SQLException e) {
+            log.log(Level.SEVERE, "Finally Statement failed", e);
+        }
+        closeConnection();
+    }
+
     protected void finallyStatement(PreparedStatement prep) {
         try {
             if (!connection.getAutoCommit()) {
---
+++ src/main/java/no/ntnu/stud/minvakt/database/DatabaseConfiguration.java
@@ -0,0 +1,60 @@
+package no.ntnu.stud.minvakt.database;
+
+import no.ntnu.stud.minvakt.jersey.MinVaktApp;
+import no.ntnu.stud.minvakt.util.TravisUtil;
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.apache.tomcat.jdbc.pool.PoolProperties;
+
+import java.lang.invoke.MethodHandles;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+/**
+ * Created by Audun on 27.01.2017.
+ */
+public class DatabaseConfiguration {
+    private static final Logger log = Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
+
+    private static final DataSource dataSource;
+
+    static {
+        PoolProperties p = new PoolProperties();
+        p.setDriverClassName("com.mysql.cj.jdbc.Driver");
+
+        if (TravisUtil.isTravis()) {
+            p.setUrl("jdbc:mysql://localhost/test");
+            p.setUsername("root");
+            p.setPassword("");
+        } else {
+            p.setUrl(MinVaktApp.getAppProperties().getProperty("mysql.url"));
+            p.setUsername(MinVaktApp.getAppProperties().getProperty("mysql.user"));
+            p.setPassword(MinVaktApp.getAppProperties().getProperty("mysql.password"));
+        }
+
+        p.setJmxEnabled(true);
+        p.setTestWhileIdle(false);
+        p.setTestOnBorrow(true);
+        p.setValidationQuery("SELECT 1");
+        p.setTestOnReturn(false);
+        p.setValidationInterval(30000);
+        p.setTimeBetweenEvictionRunsMillis(30000);
+        p.setMaxActive(20);
+        p.setInitialSize(1);
+        p.setMaxWait(10000);
+        p.setRemoveAbandonedTimeout(60);
+        p.setMinEvictableIdleTimeMillis(30000);
+        p.setMinIdle(0);
+        p.setLogAbandoned(true);
+        p.setRemoveAbandoned(true);
+        p.setJdbcInterceptors(
+                "org.apache.tomcat.jdbc.pool.interceptor.ConnectionState;" +
+                        "org.apache.tomcat.jdbc.pool.interceptor.StatementFinalizer");
+        dataSource = new DataSource();
+        dataSource.setPoolProperties(p);
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+}
