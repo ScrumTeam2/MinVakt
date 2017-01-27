e8ee5162526639c9e422b2d0c24a0fb8a61a51c2
Even Dalen <even.dalen@live.no>
2017-01-27 12:47:28 +0100

    work with edit shift from newsfeed

--- src/main/webapp/js/editShift.js
+++ src/main/webapp/js/editShift.js
@@ -13,6 +13,8 @@ var shiftTypes = {"DAY" : "Dagvakt", "EVENING" : "Kveldsvakt", "NIGHT" : "Nattev
 var $list = $('.list');
 
 var shiftId = getUrlParameter("id");
+var feedId = getUrlParameter("feedId");
+var userId = getUrlParameter("userId");
 
 $(document).ready(function() {
     $staffNumInput = $('#staffNum');
@@ -63,7 +65,7 @@ function showShiftInfo(data) {
 
     var output = `<div class="container-title">
                 <h3>${convertDate(data.date)} - ${shiftTypes[data.type]}</h3>
-            </div>`;
+                </div>`;
 
 
     $saveButton.click(performSave);
@@ -75,8 +77,15 @@ function showShiftInfo(data) {
     while(counter < data.staffNumb) {
         if (counter < data.shiftUsers.length) {
             var user = data.shiftUsers[counter];
-            output += `<div class="watch">
-                    <div class="watch-info">
+            if(feedId && feedId > 0 && userId == user.userId){
+                output += "<div class='watch green' data-category='"+user.userCategory+"'>";
+            }
+            else{
+                output += '<div class="watch" data-category="'+user.userCategory+'">';
+                feedId = 0;
+                userId = 0;
+            }
+            output += `<div class="watch-info">
                         <p class="lead">${user.userName}</p>
                         <p class="sub">${userTypes[user.userCategory]}</p>
                     </div>
@@ -91,7 +100,7 @@ function showShiftInfo(data) {
                     <div class="watch-info">
                         <p>Ledig</p>
                     </div>
-                    <a href="/html/add-employee.html?shift=${data.id}" class="link">Legg til</a>
+                    <a href="/html/add-employee.html?shift=${data.id}&feedId=${feedId}&userId=" class="link">Legg til</a>
                 </div>`;
         }
         counter++;
@@ -110,6 +119,8 @@ function showShiftInfo(data) {
             }
         });
     });
+    var percentages = updateCategoryRatio();
+    createStatusHtml(percentages);
 }
 
 function initPopup() {
@@ -126,4 +137,54 @@ function initPopup() {
     $('#userCloseBtn').click(function() {
         $popup.hide();
     });
+}
+function updateCategoryRatio() {
+    var staffCount = $staffNumInput.val();
+    var assistantCount = 0;
+    var healthWorkerCount = 0;
+    var nurseCount = 0;
+    var elements = $(".watch");
+    for(var i = 0;i<elements.length;i++){
+        switch ($(elements[i]).attr("data-category")){
+            case 'ASSISTANT':
+                assistantCount++;
+                break;
+            case 'NURSE':
+                nurseCount++;
+                break;
+            case 'HEALTH_WORKER':
+                healthWorkerCount++;
+                break;
+        }
+    }
+
+    //Assistant percentage, Nurse percentage, Health Worker percentage
+    return [assistantCount / staffCount,
+            nurseCount / staffCount,
+        healthWorkerCount / staffCount];
+}
+function createStatusHtml(percentages) {
+    var $divElement = $("#shift-status");
+    $divElement.text("");
+    var html = "";
+    console.log(percentages);
+
+    //Assistants
+    html += "<p class=''>Assistenter: "+Math.round(percentages[0]*100)+"%</p>";
+
+    //Nurses
+    if(percentages[1] < 0.20){
+        html += "<p class='red'>Sykepleiere: "+Math.round(percentages[1]*100)+"%</p>";
+    }
+    else{
+        html += "<p class=''>Sykepleiere: "+Math.round(percentages[1]*100)+"%</p>";
+    }
+    //Health workers
+    if(percentages[2] < 0.30){
+        html += "<p class='red'>Helsefagarbeidere: "+Math.round(percentages[2]*100)+"%</p>";
+    }
+    else {
+        html += "<p class=''>Helsefagarbeidere: " + Math.round(percentages[2] * 100) + "%</p>";
+    }
+    $divElement.append(html);
 }
