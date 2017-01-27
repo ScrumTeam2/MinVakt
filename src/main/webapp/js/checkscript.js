8d3f742ecbcb459de34bac8452e33fc22d73ea7a
Audun Arbo <audunwar@student.hist.no>
2017-01-27 14:53:27 +0100

    Client now logs out when getting NOT_AUTHORIZED response

--- src/main/webapp/js/checkscript.js
+++ src/main/webapp/js/checkscript.js
@@ -2,7 +2,15 @@
  * Created by ingvildbroen on 12.01.2017.
  */
 
-$(document).ready(function(){
+$(document).ajaxError(function (event, jqXHR) {
+    // Called when we get an AJAX-error anywhere on the site
+    if (jqXHR.status == 401) {
+        // Not authorized, log out
+        clearLogin();
+    }
+});
+
+$(document).ready(function () {
     addNameToTop();
     var currPage = window.location.pathname.replace("/html/", "");
     var $menu = $('#popup_menu');
@@ -10,30 +18,30 @@ $(document).ready(function(){
 
     for (var i = 0; i < anchors.length; i++) {
         var $link = $(anchors[i]);
-        if($link.attr("href") === currPage) {
+        if ($link.attr("href") === currPage) {
             $link.addClass("active");
         }
     }
 
-    if(sessionStorage.getItem("SessionId")){
+    if (sessionStorage.getItem("SessionId")) {
         var expire = sessionStorage.getItem("SessionExpires");
         var date = new Date();
         var timeNow = date.getTime();
 
         var userCategory = sessionStorage.getItem("SessionIdCat");
-        if(userCategory === 'ADMIN'){
+        if (userCategory === 'ADMIN') {
             console.log("admin");
             adminAccess();
-        } else if(userCategory === 'HEALTH_WORKER' || userCategory === 'ASSISTANT' || userCategory === 'NURSE'){
+        } else if (userCategory === 'HEALTH_WORKER' || userCategory === 'ASSISTANT' || userCategory === 'NURSE') {
             console.log("assistant, nurse or health");
             console.log("path ", window.location.pathname);
             employeeAccess();
-        } else{
+        } else {
             console.log("category not found", userCategory);
             clearLogin();
         }
 
-        if(expire <= timeNow){
+        if (expire <= timeNow) {
             $.ajax({
                 url: "/rest/session/check",
                 type: 'GET',
@@ -41,13 +49,13 @@ $(document).ready(function(){
                 error: clearLogin
             });
         }
-    } else{
+    } else {
         clearLogin();
     }
 });
 
 //Menu button
-$('#menu_icon').on("click", function(e) {
+$('#menu_icon').on("click", function (e) {
     e.preventDefault();
     var $menu = $('#popup_menu');
     $menu.toggle();
@@ -58,7 +66,7 @@ $('#menu_icon').on("click", function(e) {
 });
 
 //More button
-$('#more_icon').on("click", function(e) {
+$('#more_icon').on("click", function (e) {
     e.preventDefault();
     var $more = $('#popup_more');
     $more.toggle();
@@ -68,12 +76,12 @@ $('#more_icon').on("click", function(e) {
     }
 });
 
-$('#logout').on("click", function(e){
+$('#logout').on("click", function (e) {
     e.preventDefault();
     logOut();
 });
 
-function logOut(){
+function logOut() {
     $.ajax({
         url: "/rest/session/log_out",
         type: 'GET',
@@ -83,14 +91,14 @@ function logOut(){
 }
 
 
-function renewSession(){
+function renewSession() {
     //console.log("renew");
     var date = new Date();
     date.setTime(date.getTime() + (2 * 60 * 60 * 1000));
     sessionStorage.SessionExpires = date;
 }
 
-function clearLogin(){
+function clearLogin() {
     //console.log("log out");
     //console.log("login again");
     sessionStorage.clear();
@@ -98,31 +106,31 @@ function clearLogin(){
 }
 
 /*
-* Ansatt:
-* Ansattinfo (begrenset)                -- user-e.html
-* Bytte vakt                            --
-* Fravær/sykdom                         --
-* Registrere tilgjengelighet            -- availability.html
-* Registrere overtid i timebank         --
-* Egne vakter                           -- home-e.html
-*
-* Admin:
-* Registrer bruker                      -- register.html
-* Ansattinfo (alt)                      -- user-a.html
-* Tilgjengelighet for hver ansatt       --
-* Rediger vakt (ansvarsvakt og ansatte) --
-* Godkjenne vaktbytte (feed og vanlig?) -- home-a.html
-* Lage vaktliste                        -- new-shift.html
-* Godkjenne overtid i timebank          --
-* "Godkjenne" sykdom - feed             -- home-a.html
-* Sende inn vaktliste                   --
-*
-* Alle:
-* login                                 -- login.html
-* endre passord                         -- resetpassword.html
-* generell vaktliste med info           --
-* se ledige vakter                      --
-* */
+ * Ansatt:
+ * Ansattinfo (begrenset)                -- user-e.html
+ * Bytte vakt                            --
+ * Fravær/sykdom                         --
+ * Registrere tilgjengelighet            -- availability.html
+ * Registrere overtid i timebank         --
+ * Egne vakter                           -- home-e.html
+ *
+ * Admin:
+ * Registrer bruker                      -- register.html
+ * Ansattinfo (alt)                      -- user-a.html
+ * Tilgjengelighet for hver ansatt       --
+ * Rediger vakt (ansvarsvakt og ansatte) --
+ * Godkjenne vaktbytte (feed og vanlig?) -- home-a.html
+ * Lage vaktliste                        -- new-shift.html
+ * Godkjenne overtid i timebank          --
+ * "Godkjenne" sykdom - feed             -- home-a.html
+ * Sende inn vaktliste                   --
+ *
+ * Alle:
+ * login                                 -- login.html
+ * endre passord                         -- resetpassword.html
+ * generell vaktliste med info           --
+ * se ledige vakter                      --
+ * */
 
 
 var adminlinks = [
@@ -158,28 +166,28 @@ var employeelinks = [
     "/html/register-overtime.html"
 ];
 
-function adminAccess(){
+function adminAccess() {
     var currentLocation = window.location.pathname;
     //console.log("location: ", currentLocation);
 
     var found = $.inArray(currentLocation, adminlinks) > -1;
 
-    if(found){
+    if (found) {
         //console.log("OK a");
-    } else{
+    } else {
         //console.log("FAIL for A go to");
         window.location.replace("/html/index.html");
     }
 }
 
-function employeeAccess(){
+function employeeAccess() {
     var currentLocation = window.location.pathname;
     //console.log("location: ", currentLocation);
 
     var found = $.inArray(currentLocation, employeelinks) > -1;
-    if(found){
+    if (found) {
         //console.log("OK e");
-    } else{
+    } else {
         //console.log("FAIL for E go to");
         window.location.replace("/html/index.html");
     }
@@ -187,25 +195,27 @@ function employeeAccess(){
 
 
 // URL PARAMETERS
-function setUrlParameter(key, value)
-{
-    key = encodeURI(key); value = encodeURI(value);
+function setUrlParameter(key, value) {
+    key = encodeURI(key);
+    value = encodeURI(value);
 
     var kvp = document.location.search.substr(1).split('&');
 
-    var i=kvp.length; var x; while(i--)
-{
-    x = kvp[i].split('=');
+    var i = kvp.length;
+    var x;
+    while (i--) {
+        x = kvp[i].split('=');
 
-    if (x[0]==key)
-    {
-        x[1] = value;
-        kvp[i] = x.join('=');
-        break;
+        if (x[0] == key) {
+            x[1] = value;
+            kvp[i] = x.join('=');
+            break;
+        }
     }
-}
 
-    if(i<0) {kvp[kvp.length] = [key,value].join('=');}
+    if (i < 0) {
+        kvp[kvp.length] = [key, value].join('=');
+    }
 
     //Set back url
     window.history.replaceState('', '', window.location.href.split("?")[0] + "?" + kvp.join('&'));
@@ -226,7 +236,7 @@ var getUrlParameter = function getUrlParameter(sParam) {
     }
 };
 
-function convertDate(dateInput){
+function convertDate(dateInput) {
     var monthNames = [
         "januar", "februar",
         "mars", "april", "mai",
